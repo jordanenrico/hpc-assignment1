488361c18dfb5808bd7e3ba8cf235f9f43c68cff
jordanenrico <56438955+jordanenrico@users.noreply.github.com>
2021-04-24 06:54:55 +0700

    Update galsim.c
    More memory-efficient

--- galsim.c
+++ galsim.c
@@ -6,37 +6,27 @@
 #include "graphics.h"
 #include "file_operations.h"
 
-double distance(double xMajor, double yMajor, double xMinor, double yMinor){
-    return sqrt(pow(xMajor-xMinor, 2)+pow(yMajor-yMinor,2));
-}
+int calcAccel(int N, const int G, double* mass, double* xPos, double* yPos, double* xAccelNew, double* yAccelNew){
+    /* Fills up xAccelNew and yAccelNew with magnitude and direction of the acceleration.
+       xPotentSum = x component of the sum of mass_j*rVec/r^3 values experienced by planet i from planet j.
+       yPotentSum = y component of the sum of mass_j*rVec/r^3 values experienced by planet i from planet j. */
 
-int animate(char* name, double* xPos, double* yPos, int N, int nsteps){
-    
-    const float circleRadius=0.0025, circleColor=0;
-    const int windowWidth=800;
-    
-    float L=1, W=1;
-    int i = 0;
-    
-    InitializeGraphics(name,windowWidth,windowWidth);
-    SetCAxes(0,1);
-    printf("Hit q to quit.\n");
+    const double epsilon = 0.001;
+    double magnitude, xPotentSum, yPotentSum;
 
-    while (!CheckForQuit()){
-        i++;
-        i %= nsteps+1;
-
-        ClearScreen();
-        int j;
-        for (j=0; j<N; j++){
-            DrawCircle(xPos[j*(nsteps+1)+i], yPos[j*(nsteps+1)+i], L, W, circleRadius, circleColor);
+    for (int i=0; i<N; i++){
+        xPotentSum = 0;
+        yPotentSum = 0;
+        for (int j=0; j<N; j++){
+            if (i != j){
+                magnitude   = sqrt(pow(xPos[i] - xPos[j], 2) + pow(yPos[i] - yPos[j], 2)) + epsilon;
+                xPotentSum += mass[j]*(xPos[i] - xPos[j])/pow(magnitude, 3);
+                yPotentSum += mass[j]*(yPos[i] - yPos[j])/pow(magnitude, 3);
+            }
         }
-        Refresh();
-
-        usleep(2000);
+        xAccelNew[i] = -G*xPotentSum;
+        yAccelNew[i] = -G*yPotentSum;
     }
-    FlushDisplay();
-    CloseDisplay();
     return 0;
 }
 
@@ -46,20 +36,17 @@ int main(int argc, char *argv[]){
         return -1;
     }
 
-    int i, j, k; // iterators
-
-    int N = atoi(argv[1]);
+    int N            = atoi(argv[1]);
     char filename[50];
     strcpy(filename, argv[2]);
-    int nsteps = atoi(argv[3]);
-    double delta_t = atof(argv[4]);
+    int nsteps       = atoi(argv[3]);
+    double delta_t   = atof(argv[4]);
     int withGraphics = atoi(argv[5]);
+    const double G   = 100.0/N;
+    int nData        = N*6;
+    double* rawData  = (double*)malloc(nData*sizeof(double));
 
-    const double G = 100/N;
-    const double epsilon = 0.001;
-
-    int nData = N*6;
-    double rawData[nData];
+    /* reading initial conditions from a file*/
     int isRead = read_doubles_from_file(nData, rawData, filename);
 
     /* checking whether data has been imported correctly or not */
@@ -76,84 +63,75 @@ int main(int argc, char *argv[]){
 
     /*  xPos[i*(nsteps+1)+j] is the x position of planet i at timestep j.
         We use (nsteps+1) to store nsteps data and the initial condition. */
-    double* xPos   = (double*)malloc(N*(nsteps+1)*sizeof(double));
-    double* yPos   = (double*)malloc(N*(nsteps+1)*sizeof(double));
-    double* xSpeed = (double*)malloc(N*(nsteps+1)*sizeof(double));
-    double* ySpeed = (double*)malloc(N*(nsteps+1)*sizeof(double));
+    double* xPos   = (double*)malloc(N*sizeof(double));
+    double* yPos   = (double*)malloc(N*sizeof(double));
+    double* xSpeed = (double*)malloc(N*sizeof(double));
+    double* ySpeed = (double*)malloc(N*sizeof(double));
     double* mass   = (double*)malloc(N*sizeof(double));
     double* bright = (double*)malloc(N*sizeof(double));
 
-    /* inserting initial data into arrays */
-    for (i=0; i<N; i++){
-        xPos[i*(nsteps+1)+0]   = rawData[i*6+0];
-        yPos[i*(nsteps+1)+0]   = rawData[i*6+1];
-        xSpeed[i*(nsteps+1)+0] = rawData[i*6+3];
-        ySpeed[i*(nsteps+1)+0] = rawData[i*6+4];
-
+    /* inserting initial data into new arrays */
+    for (int i=0; i<N; i++){
+        xPos[i]   = rawData[i*6+0];
+        yPos[i]   = rawData[i*6+1];
         mass[i]   = rawData[i*6+2];
+        xSpeed[i] = rawData[i*6+3];
+        ySpeed[i] = rawData[i*6+4];
         bright[i] = rawData[i*6+5];
     }
 
-    double xMassPerArea; //stores the x component of the sum of m/r^2
-    double yMassPerArea; //stores the y component of the sum of m/r^2
-    double majorMass;    //the mass of an object on which the gravity acts
-    double majorXPos;    //the x position of majorMass
-    double majorYPos;    //the y position of majorMass
-    double minorMass;    //the mass of an object which affects majorMass
-    double minorXPos;    //the x position of minorMass
-    double minorYPos;    //the y position of minorMass
-    double accelX;       //the x component of majorMass acceleration caused by gravity
-    double accelY;       //the y component of majorMass acceleration caused by gravity
-    double newXSpeed, newYSpeed, newXPos, newYPos;
+    /* empty arrays to store acceleration information */
+    double* xAccelNew = (double*)malloc(N*sizeof(double));
+    double* yAccelNew = (double*)malloc(N*sizeof(double));
 
     /* SIMULATION */
-    for (k=0; k<nsteps; k++){
-        for (i=0; i<N; i++){
-            xMassPerArea = 0;
-            yMassPerArea = 0;
-            majorMass = mass[i];
-            majorXPos = xPos[i*(nsteps+1)+k];
-            majorYPos = yPos[i*(nsteps+1)+k];
+    const float circleRadius = 0.0025, circleColor = 0;
+    const int windowWidth = 800;
+    float L = 1, W = 1;
+    int i = 0;
+    if (withGraphics){
+        InitializeGraphics(argv[0],windowWidth,windowWidth);
+        SetCAxes(0,1);
+    }
+    
+    for (int k=0; k<nsteps; k++){
+        calcAccel(N, G, mass, xPos, yPos, xAccelNew, yAccelNew);
+        for (int i=0; i<N; i++){
 
-            for (j=0; j<N; j++){
-                if (i!=j){
-                    minorMass = mass[j];
-                    minorXPos = xPos[j*(nsteps+1)+k];
-                    minorYPos = yPos[j*(nsteps+1)+k];
+            xSpeed[i] += xAccelNew[i]*delta_t;
+            ySpeed[i] += yAccelNew[i]*delta_t;
 
-                    xMassPerArea += minorMass/pow(distance(majorXPos, majorYPos, minorXPos, minorYPos) + epsilon, 3)*(majorXPos-minorXPos);
-                    yMassPerArea += minorMass/pow(distance(majorXPos, majorYPos, minorXPos, minorYPos) + epsilon, 3)*(majorYPos-minorYPos);
-                }
+            xPos[i] += xSpeed[i]*delta_t;
+            yPos[i] += ySpeed[i]*delta_t;
+        }
+
+        if (withGraphics){
+            ClearScreen();
+            for (int i=0; i<N; i++){
+                DrawCircle(xPos[i], yPos[i], L, W, circleRadius, circleColor);
             }
+            Refresh();
+            usleep(1000);
+        }
 
-            accelX = -G*majorMass*xMassPerArea;
-            accelY = -G*majorMass*yMassPerArea;
-
-            newXSpeed = xSpeed[i*(nsteps+1)+k]+accelX*delta_t; //v'=v+at
-            newYSpeed = ySpeed[i*(nsteps+1)+k]+accelY*delta_t;
-            xSpeed[i*(nsteps+1)+k+1] = newXSpeed;
-            ySpeed[i*(nsteps+1)+k+1] = newYSpeed;
-
-            newXPos = xPos[i*(nsteps+1)+k]+newXSpeed*delta_t; //s'=s+vt
-            newYPos = yPos[i*(nsteps+1)+k]+newYSpeed*delta_t;
-            xPos[i*(nsteps+1)+k+1] = newXPos;
-            yPos[i*(nsteps+1)+k+1] = newYPos;
+        if ((k+1)%((int)(nsteps/15)) == 0){
+            printf("Progress: %3.1f %%.\n", ((double)k/nsteps)*100);
         }
     }
-
-    /* ANIMATION */
     if (withGraphics){
-        animate(argv[0], xPos, yPos, N, nsteps);
+        FlushDisplay();
+        CloseDisplay();
     }
+    printf("Progress: COMPLETE\n");
 
-    /* PREPARING DATA TO BE SAVED */
-    double* output = (double*)malloc(N*6*sizeof(double));
-    for (i=0; i<N; i++){
-        output[i*6+0] = xPos[i*(nsteps+1)+nsteps];
-        output[i*6+1] = yPos[i*(nsteps+1)+nsteps];
+    /* preparing data for saving */
+    double* output = (double*)malloc(nData*sizeof(double));
+    for (int i=0; i<N; i++){
+        output[i*6+0] = xPos[i];
+        output[i*6+1] = yPos[i];
         output[i*6+2] = mass[i];
-        output[i*6+3] = xSpeed[i*(nsteps+1)+nsteps];
-        output[i*6+4] = ySpeed[i*(nsteps+1)+nsteps];
+        output[i*6+3] = xSpeed[i];
+        output[i*6+4] = ySpeed[i];
         output[i*6+5] = bright[i];
     }
 
@@ -162,16 +140,20 @@ int main(int argc, char *argv[]){
     if (isWritten ==0){
         printf("Output successfully written to \"output.gal\"\n.");
     }
-    else{
+    else {
         printf("Writing failed\n.");
     }
 
+    /* freeing memory */
+    free(rawData);
     free(xPos);
     free(yPos);
     free(xSpeed);
     free(ySpeed);
     free(mass);
     free(bright);
+    free(xAccelNew);
+    free(yAccelNew);
     free(output);
 
     return 0;
